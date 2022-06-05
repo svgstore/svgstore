2bb72e947e2f29c7008e6065eccaaa1a688a06fb
Qrac <qrac.jp@gmail.com>
2022-06-05 16:36:48 +0900

    Add types

---
+++ types/index.d.ts
@@ -0,0 +1,25 @@
+declare type Options = {
+	cleanDefs?: boolean | string[];
+	cleanSymbols?: boolean | string[];
+	inline?: boolean;
+	svgAttrs?:
+		| boolean
+		| {
+				[key: string]: string;
+		  };
+	symbolAttrs?:
+		| boolean
+		| {
+				[key: string]: string;
+		  };
+	copyAttrs?: boolean | string[];
+	renameDefs?: boolean;
+};
+
+declare function svgstore(options: Options): {
+	element: import('cheerio').CheerioAPI;
+	add: (id: string, file: string, options: Options) => any;
+	toString: (options: Options) => string;
+};
+
+export default svgstore;
